d5b93e516cabca87ef4236c70c6d076605f2c7a3
KhoiVuMinh2412 <khoi.vuminh241207@hcmut.edu.vn>
2026-02-23 11:42:44 +0700

    create loop to read can and delete the pointers

--- src/mecanum_driver/src/mecanum_driver.cpp
+++ src/mecanum_driver/src/mecanum_driver.cpp
@@ -18,62 +18,68 @@ class MecanumDriver : public rclcpp::Node {
     // create a can communication port 
     // create a robot consisting of 4 mecanum wheels
 
-    // CanComm* can_comm;
-    // MecanumRobot* robot;
+    CanComm* can_comm;
+    MecanumRobot* robot;
 
-    std::unique_ptr<CanComm> can_comm_;
-
-    std::unique_ptr<ODriveMotor> fl_;
-    std::unique_ptr<ODriveMotor> fr_; 
-    std::unique_ptr<ODriveMotor> rl_; 
-    std::unique_ptr<ODriveMotor> rr_;
-
-    std::unique_ptr<MecanumRobot> robot_;
 
     // 4 mecanum wheels (motor) here
-    // ODriveMotor* fl; // front left
-    // ODriveMotor* fr; // front right
-    // ODriveMotor* rl; // rear left
-    // ODriveMotor* rr; // rear right
+    ODriveMotor* fl; // front left
+    ODriveMotor* fr; // front right
+    ODriveMotor* rl; // rear left
+    ODriveMotor* rr; // rear right
+
+    rclcpp::TimerBase::SharedPtr feedback_timer; // timer for listening to odrive feedback
 
     public:
     MecanumDriver()
     : Node("mecanum_driver_node")
     {
-        // can_comm = new CanComm();
-        can_comm_ = std::make_unique<CanComm>();
-        can_comm_->init("vcan0");
-
-        fl_ = std::make_unique<ODriveMotor>(0, can_comm_.get());
-        fr_ = std::make_unique<ODriveMotor>(1, can_comm_.get());
-        rl_ = std::make_unique<ODriveMotor>(2, can_comm_.get());
-        rr_ = std::make_unique<ODriveMotor>(3, can_comm_.get());
-
-        robot_ = std::make_unique<MecanumRobot>(fl_.get(), fr_.get(), rl_.get(), rr_.get());
+        can_comm = new CanComm();
+        if (!can_comm->init("vcan0")) 
+        {
+            RCLCPP_ERROR(this->get_logger(), "Khong the mo cong can vcan0~\n");
+        }
         // allocate 4 new odrive wheels with different ids
-        // fl = new ODriveMotor(0, can_comm);
-        // fr = new ODriveMotor(1, can_comm);
-        // rl = new ODriveMotor(2, can_comm);
-        // rr = new ODriveMotor(3, can_comm);
+        fl = new ODriveMotor(0, can_comm);
+        fr = new ODriveMotor(1, can_comm);
+        rl = new ODriveMotor(2, can_comm);
+        rr = new ODriveMotor(3, can_comm);
 
-        // robot = new MecanumRobot(fl, fr, rl, rr);
-        
+        robot = new MecanumRobot(fl, fr, rl, rr);
 
+        feedback_timer = this->create_wall_timer(5ms, std::bind(&MecanumDriver::read_can_loop, this));
         subscription_ = this->create_subscription<geometry_msgs::msg::Twist>("/cmd_vel", 10, std::bind(&MecanumDriver::robot_callback, this, _1));
         RCLCPP_INFO(this->get_logger(), "the mecanum node started ok!\n");
     };
         
     ~MecanumDriver(){
-        if(fl_) fl_->setAxisState(OdriveAxisState::IDLE);
-        if(fr_) fr_->setAxisState(OdriveAxisState::IDLE);
-        if(rl_) rl_->setAxisState(OdriveAxisState::IDLE);
-        if(rr_) rr_->setAxisState(OdriveAxisState::IDLE);
+        if(fl) fl->setAxisState(OdriveAxisState::IDLE);
+        if(fr) fr->setAxisState(OdriveAxisState::IDLE);
+        if(rl) rl->setAxisState(OdriveAxisState::IDLE);
+        if(rr) rr->setAxisState(OdriveAxisState::IDLE);
 
-        // delete robot;
-        // delete fl; delete fr; delete rl; delete rr;
-        // delete can_comm;
+        delete robot;
+        delete fl; delete fr; delete rl; delete rr;
+        delete can_comm;
     }
-    
+
+
+    void read_can_loop() 
+    {
+        struct can_frame frame;
+
+        while (can_comm->receive_frame(frame))
+        {
+            fl->parseCanMessage(frame.can_id, frame.data, frame.can_dlc);
+            fr->parseCanMessage(frame.can_id, frame.data, frame.can_dlc);
+            rl->parseCanMessage(frame.can_id, frame.data, frame.can_dlc);
+            rr->parseCanMessage(frame.can_id, frame.data, frame.can_dlc);
+            
+        }
+        
+    }
+
+
     void robot_callback(const geometry_msgs::msg::Twist & msg)
     {
         float vx = msg.linear.x;
@@ -82,8 +88,8 @@ class MecanumDriver : public rclcpp::Node {
 
         RCLCPP_INFO(this->get_logger(), "Received: vx = %.2f, vy = %.2f, vz     = %.2f", vx, vy, vz);
 
-        if (robot_!=nullptr) {
-        robot_->drive(vx, vy, vz);
+        if (robot!=nullptr) {
+        robot->drive(vx, vy, vz);
         }
     }
 
